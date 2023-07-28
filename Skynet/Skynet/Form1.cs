eb1daa6e3d748cf1b39dd06a751c4a88b7c2ea4c
rogervisualstudio <rogervisualstudio@yahoo.com.br>
2023-07-28 10:18:51 -0300

    It adds the initial code

--- Skynet/Skynet/Form1.cs
+++ Skynet/Skynet/Form1.cs
@@ -9,7 +9,16 @@ namespace Skynet
 
         private void ButtonRunAds_Click(object sender, EventArgs e)
         {
-
+            if(RichTextBoxMessage.Text == "")
+            {
+                MessageBox.Show("Digite a mensagem que deseja enviar aos grupos e contatos no WhatsApp!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                RichTextBoxMessage.Focus();
+            }
+            if(ListBoxGroups.Items.Count == 0) 
+            {
+                MessageBox.Show("Você não adicionou nenhum grupo ou contato para enviar as mensagens, por favor, preencha a lista de contatos e grupos.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TextBoxGroupName.Focus();
+            }
         }
 
         private void ListBoxGroups_SelectedIndexChanged(object sender, EventArgs e)
@@ -22,6 +31,13 @@ namespace Skynet
 
         private void ButtonAddGroup_Click(object sender, EventArgs e)
         {
+            if(TextBoxGroupName.Text == "")
+            {
+                MessageBox.Show("Digite o nome do grupo ou contato para adicionar na lista!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TextBoxGroupName.Focus();
+            }
+            TextBoxGroupName.Clear();
+            ListBoxGroups.Items.Add(TextBoxGroupName.Text);
         }
     }
 }
