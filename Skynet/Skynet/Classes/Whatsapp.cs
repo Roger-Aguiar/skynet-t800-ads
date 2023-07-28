c13232ce5c6594f3fcc9a6dd9477e5c34c7ff331
rogervisualstudio <rogervisualstudio@yahoo.com.br>
2023-07-28 10:29:55 -0300

    It installs the required packages and creates the Whatsapp class.

---
+++ Skynet/Skynet/Classes/Whatsapp.cs
@@ -0,0 +1,25 @@
+﻿namespace Skynet.Classes
+{
+    public class Whatsapp : Web
+    {
+        public List<string>? Contacts { get; set; } = new();
+        public string? Message { get; set; }
+
+        public void SendMessage()
+        {
+            foreach (var contact in Contacts)
+            {
+                var web = new Web();
+                web.StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\roger\\AppData\\Local\\Google\\Chrome\\User Data");
+                web.Navigate("https://web.whatsapp.com/");
+                web.WaitForLoad();
+                Thread.Sleep(TimeSpan.FromSeconds(15));
+                var search = web.AssignValue(TypeElement.Xpath, "//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div/p", contact, 6);
+                search.element.SendKeys(OpenQA.Selenium.Keys.Enter);
+                var messageGroup = web.AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p", Message);
+                messageGroup.element.SendKeys(OpenQA.Selenium.Keys.Enter);
+                web.CloseBrowser();
+            }            
+        }
+    }
+}
